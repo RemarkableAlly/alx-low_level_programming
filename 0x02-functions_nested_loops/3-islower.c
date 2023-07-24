35a28b2c5c6e42c37bc05b81200ec17846232743
RemarkableAlly <RemarkableAlly>
2023-07-24 02:41:11 -0700

    a function that checks for lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *  islower - Return 1 if lowercase and return 0 if otherwise
+ *  _islower - Return 1 if lowercase and return 0 if otherwise
  *
  *  Return: Always return 0
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <=122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
