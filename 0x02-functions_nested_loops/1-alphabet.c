36c72259ea9a4a47d5b5ab6827f076852ed5cf81
RemarkableAlly <RemarkableAlly>
2023-07-23 23:30:48 -0700

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print out alphabets
+ *
+ * Return: void
+ */
+
+void  print_alphabet(void)
+{
+	char alphabets;
+
+	alphabets = 'a';
+
+	while (alphabets < 'z')
+	{
+		_putchar(alphabets);
+		alphabets++;
+	}
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,6 @@
 read me file for 0x02-functions_nested_loops
+
+created a main.h file (header), places all the prototypes in the file
+
+0-putchar.c | printing the word _putchar
+1-alphabet.c | a function that prints the alphabet, in lowercase
