d7cd3a1ab3c91eb19928c8ae0545eaa525111d39
RemarkableAlly <xradebekhaya@gmail.com>
2023-08-03 14:51:53 -0700

    fixing the main

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,30 @@
 #include "main.h"
-
 /**
- * _strncpy - concatenate strings, defining the size of second string.
- * @dest: string with concatenation
- * @src: string to be concatenated
- * @n: size of second string
- * Return: Always 0.
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int x;
+	int i;
+	int j;
 
-	for (x = 0; x < n && src[x] != '\0'; x++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		*(dest + x) = *(src + x);
+		i++;
 	}
-	for ( ; x < n; x++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		*(dest + x) = '\0';
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
-	_putchar('\n');
+	dest[i] = '\0';
 	return (dest);
 }
